d7f6930e972bb51c4c716589245d11f97bfa0731
Staphany Park <stapark008@gmail.com>
2017-04-24 04:37:11 -0700

    Simulate CLI with placeholder implementation.

--- src/cli/cli.cc
+++ src/cli/cli.cc
@@ -2,22 +2,84 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <iostream>
-#include <cstring>
+#include <iostream>  // std::cout
+#include <vector>  // std::vector
+#include <boost/tokenizer.hpp>  // boost::tokenizer, boost::escaped_list_separator
+#include <iomanip>  // std::setw, std::setfill
+#include <map>  // std::map
 
-#include "boost/tokenizer.hpp"
+void welcome() {
+  const char * logo =
+    " ______   ______   ______   ______   __  __   _____    ______    \n"
+    "/\\  == \\ /\\  ___\\ /\\  == \\ /\\  == \\ /\\ \\_\\ \\ /\\  __-. /\\  == \\   \n"
+    "\\ \\  __< \\ \\  __\\ \\ \\  __< \\ \\  __< \\ \\____ \\\\ \\ \\/\\ \\\\ \\  __<   \n"
+    " \\ \\_____\\\\ \\_____\\\\ \\_\\ \\_\\\\ \\_\\ \\_\\\\/\\_____\\\\ \\____- \\ \\_____\\ \n"
+    "  \\/_____/ \\/_____/ \\/_/ /_/ \\/_/ /_/ \\/_____/ \\/____/  \\/_____/ \n";
+  std::cout << logo << std::endl;
+  std::cout << "Welcome to BerryDB!" << std::endl;
+}
+
+void print_prompt(int line) {
+  std::cout << "berrydb 🍓 :" << std::setw(3) << std::setfill('0') << line << "> ";
+}
+
+void print_output(std::string message) {
+  std::cout << "=> " << message << std::endl;
+}
+
+typedef boost::tokenizer<boost::escaped_list_separator<char>> cli_tokenizer;
 
 int main() {
-  std::cout << "Welcome to BerryDB!" << std::endl;
+  welcome();
+
+  std::map<std::string, std::string> data;
+
+  int line = 0;
   while (true) {
-    std::cout << "berrydb> ";
+    print_prompt(++line);
+
     std::string input;
     std::getline(std::cin, input);
 
     std::string delimiter = " ";
-    std::string command = input.substr(0, input.find(delimiter));
+    std::vector<std::string> args;
+    cli_tokenizer tokenizer(input, boost::escaped_list_separator<char>('\\', ' ', '\"'));
+    for (cli_tokenizer::iterator it = tokenizer.begin(); it != tokenizer.end(); ++it) {
+      args.push_back(*it);
+    }
 
-    std::cout << "=> " << command << std::endl;
+    if (args.size() == 0) { continue; }
+    if (args[0] == "get") {
+      if (args.size() != 2) {
+        print_output("⚠️  Expected 1 argument.");
+      } else {
+        auto result = data.find(args[1]);
+        if (result != data.end()) {
+          print_output(result->second);
+        } else {
+          print_output("❌  Not found.");
+        }
+      }
+    } else if (args[0] == "set") {
+      if (args.size() != 3) {
+        print_output("⚠️  Expected 2 arguments.");
+      } else {
+        data[args[1]] = args[2];
+      }
+    } else if (args[0] == "delete") {
+      if (args.size() != 2) {
+        print_output("⚠️  Expected 1 argument.");
+      } else {
+        auto result = data.find(args[1]);
+        if (result != data.end()) {
+          data.erase(args[1]);
+        } else {
+          print_output("❌  Not found.");
+        }
+      }
+    } else {
+      print_output("⚠️  Unsupported command.");
+    }
   }
   return 0;
 }
