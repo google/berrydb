a9827d87a3bd33b5cec1c8ad0789cc4af2a7f2b0
Staphany Park <stapark008@gmail.com>
2017-04-24 03:26:31 -0700

    CLI build setup.

---
+++ src/cli/cli.cc
@@ -0,0 +1,21 @@
+// Copyright 2017 The BerryDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <iostream>
+#include <cstring>
+
+int main() {
+  std::cout << "Welcome to BerryDB!" << std::endl;
+  while (true) {
+    std::cout << "berrydb> ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::string delimiter = " ";
+    std::string command = input.substr(0, input.find(delimiter));
+
+    std::cout << "=> " << command << std::endl;
+  }
+  return 0;
+}
